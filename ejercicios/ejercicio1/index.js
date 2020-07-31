cd5ba8fc280a78e21b8101c9b9b73f174bbe059a
dafnemus <dafnemus@gmail.com>
2020-07-31 17:45:00 -0300

    ejercicio 1: ciclos

---
+++ ejercicios/ejercicio1/index.js
@@ -0,0 +1,12 @@
+/* 
+Escriba un ciclo que haga siete llamadas a console.log para generar el siguiente triángulo:
+#
+##
+###
+####
+#####
+######
+####### */
+
+for (let line = "#"; line.length < 8; line += "#")
+  console.log(line);
